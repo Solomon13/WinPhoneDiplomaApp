6047d0d4dc945540f382e7b82f0e7bb1a5e72476
Oleksii Zherehi <alexey.zherehi@outlook.com>
2015-12-27 17:50:07 +0200

    Create a project

---
+++ WinPhoneClient/WinPhoneClient/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+namespace WinPhoneClient.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to the most relevant view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// The key used by the NavigationService to go to the second page.
+        /// </summary>
+        public const string SecondPageKey = "SecondPage";
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// This property can be used to force the application to run with design time data.
+        /// </summary>
+        public static bool UseDesignTimeData
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+        }
+    }
+}
